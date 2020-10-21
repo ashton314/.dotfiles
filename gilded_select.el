d584b1d93fe2bed177deb54f66eb677d84a31b72
Ashton Wiersdorf <ashton.wiersdorf@tazworks.com>
2020-10-21 02:01:51 -0600

    Fixup bugs with prefix arguments

--- gilded_select.el
+++ gilded_select.el
@@ -28,42 +28,52 @@ to the functions they're bound to."
 the keybinding without any frills."
   (format "(%s)" (propertize str 'face 'font-lock-doc-face)))
 
-(defun gilded-mx ()
-  "Like `execute-extended-command', but with fancy annotations."
-  (interactive)
-  (command-execute
-   (intern				; This strips off the annotations since they're all just propertized on
-    (car (split-string			; Remove the keybind annotation if present
-	  (completing-read
-	   "M-x "
-	   (let ((modes (make-hash-table :test #'equal))
-		 (keymap (make-hash-table :test #'equal))
-		 (cmds nil))
-	     (map-do
-	      (lambda (var _)
-		(when (boundp var)
-		  (puthash
-		   (or (get var :minor-mode-function) var)
-		   (propertize
-		    (symbol-name var)
-		    'face
-		    (if (symbol-value var)
-			'compilation-mode-line-exit
-		      'compilation-mode-line-fail))
-		   modes)))
-	      minor-mode-alist)
+(defun gen-times (n str acc)
+  (if (= n 0) acc (gen-times (- n 1) str (cons str acc))))
 
-	     (obarray-map
-	      (lambda (sym)
-		(when (commandp sym)
-		  (push
-		   (or (gethash sym modes)
-		       (gethash sym command-keybinding-hash)
-		       (symbol-name sym))
-		   cmds)))
-	      obarray)
-	     cmds)
-	   nil
-	   'require-match) " ")))
-   'record))
+(defun prompt-string (prefix)
+  (format "%sM-x "
+	  (if (and prefix (listp prefix) (not (= (car prefix) 0)) (= (mod (car prefix) 4) 0))
+	      (apply #'concat (gen-times (/ (car prefix) 4) "C-u " '()))
+	    (if prefix (format "(%s) " prefix) ""))))
+
+(defun gilded-mx (prefix)
+  "Like `execute-extended-command', but with fancy annotations."
+  (interactive "P")
+  (let ((current-prefix-arg prefix))
+    (call-interactively
+     (intern				; This strips off the annotations since they're all just propertized on
+      (car (split-string			; Remove the keybind annotation if present
+	    (completing-read
+	     (prompt-string prefix)
+	     (let ((modes (make-hash-table :test #'equal))
+		   (keymap (make-hash-table :test #'equal))
+		   (cmds nil))
+	       (map-do
+		(lambda (var _)
+		  (when (boundp var)
+		    (puthash
+		     (or (get var :minor-mode-function) var)
+		     (propertize
+		      (symbol-name var)
+		      'face
+		      (if (symbol-value var)
+			  'compilation-mode-line-exit
+			'compilation-mode-line-fail))
+		     modes)))
+		minor-mode-alist)
+
+	       (obarray-map
+		(lambda (sym)
+		  (when (commandp sym)
+		    (push
+		     (or (gethash sym modes)
+			 (gethash sym command-keybinding-hash)
+			 (symbol-name sym))
+		     cmds)))
+		obarray)
+	       cmds)
+	     nil
+	     'require-match) " ")))
+     'record)))
 
