7b19281950b8a5cd5e7674254923d0081abbe37d
Ashton Wiersdorf <ashton.wiersdorf@tazworks.com>
2020-10-20 23:59:28 -0600

    Fix docs, fix function to work with annotated selection

--- gilded_select.el
+++ gilded_select.el
@@ -10,13 +10,13 @@
 
 (defun rehash-key-bindings ()
   "Rebuild `command-keybinding-hash' so that calling
-\[execute-extended-command] will show the proper keybindings next
+`execute-extended-command' will show the proper keybindings next
 to the functions they're bound to."
   (interactive)
   (obarray-map
    (lambda (sym)
      (when (and (commandp sym) (where-is-internal sym))
-       (puthash sym (format "%s%s" (symbol-name sym)
+       (puthash sym (format "%s %s" (symbol-name sym)
 			    (keybind-propertize (key-description (car (where-is-internal sym)))))
 		command-keybinding-hash)
        ))
@@ -24,47 +24,52 @@ to the functions they're bound to."
   (message "Finished building keybinding cache"))
 
 (defun keybind-propertize (str)
-  (format " (%s)" (propertize str 'face 'font-lock-doc-face)))
+  "Function used to format the keybinding annotation. Receives
+the keybinding without any frills."
+  (format "(%s)" (propertize str 'face 'font-lock-doc-face)))
 
 (defun guilded-mx ()
   "Like `execute-extended-command', but with fancy annotations."
   (interactive)
   (command-execute
    (intern				; This strips off the annotations since they're all just propertized on
-    (completing-read
-     "M-x "
-     (let ((modes (make-hash-table :test #'equal))
-	   (keymap (make-hash-table :test #'equal))
-           (cmds nil))
-       (map-do
-	(lambda (var _)
-          (when (boundp var)
-            (puthash
-             (or (get var :minor-mode-function) var)
-             (propertize
-              (symbol-name var)
-              'face
-              (if (symbol-value var)
-                  'compilation-mode-line-exit
-		'compilation-mode-line-fail))
-             modes)))
-	minor-mode-alist)
+    (car (split-string			; Remove the keybind annotation if present
+	  (completing-read
+	   "M-x "
+	   (let ((modes (make-hash-table :test #'equal))
+		 (keymap (make-hash-table :test #'equal))
+		 (cmds nil))
+	     (map-do
+	      (lambda (var _)
+		(when (boundp var)
+		  (puthash
+		   (or (get var :minor-mode-function) var)
+		   (propertize
+		    (symbol-name var)
+		    'face
+		    (if (symbol-value var)
+			'compilation-mode-line-exit
+		      'compilation-mode-line-fail))
+		   modes)))
+	      minor-mode-alist)
 
-       (obarray-map
-	(lambda (sym)
-          (when (commandp sym)
-            (push
-             (or (gethash sym modes)
-		 (gethash sym command-keybinding-hash)
-		 (symbol-name sym))
-             cmds)))
-	obarray)
-       cmds)
-     nil
-     'require-match))
+	     (obarray-map
+	      (lambda (sym)
+		(when (commandp sym)
+		  (push
+		   (or (gethash sym modes)
+		       (gethash sym command-keybinding-hash)
+		       (symbol-name sym))
+		   cmds)))
+	      obarray)
+	     cmds)
+	   nil
+	   'require-match) " ")))
    'record))
 
 (define-key global-map (kbd "M-x") 'guilded-mx)
+
+(split-string "foo-bar-baz (foo)" " ")
 
 (add-hook 'after-init-hook 'rehash-key-bindings)
 
