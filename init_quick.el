269c021b577c560a7ae5426c3c8fa4dbf935d165
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2021-06-16 14:50:55 -0600

    Add notes for future

--- init_quick.el
+++ init_quick.el
@@ -1,8 +1,10 @@
-(setq gc-cons-threshold 500000000)
+(setq gc-cons-threshold 500000000)	; ~half a gig
 
 (load-file "~/.dotfiles/core.el")
 (load-file "~/.dotfiles/keybindings.el")
 (load-file "~/.dotfiles/lang-support.el")
+;; (load-file "~/.dotfiles/email-setup.el")
+;; (load-file "~/.dotfiles/org-customizations.el")
 (load-file "~/.emacs.d/device_vars.el")
 
 (setq gc-cons-threshold 800000)
